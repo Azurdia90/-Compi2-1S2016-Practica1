57883bd5ab039d2debb42e68d7d5adb6c10c4327
Cristian Azurdia <201020331@ingenieria.usac.edu.gt>
2016-02-16 23:56:17 -0600

    Versión 0.5
    - Gramática corregida casi en su totalidad

--- [Compi2]Practica1_201020331/[Compi2]Practica1_201020331/Gramatica.cs
+++ [Compi2]Practica1_201020331/[Compi2]Practica1_201020331/Gramatica.cs
@@ -13,21 +13,46 @@ namespace _Compi2_Practica1_201020331
         public Gramatica()
         {
             //1. TERMINALES
+
+            //Terminales Definidos en base a expresiones regulares
             IdentifierTerminal iden = new IdentifierTerminal("id");
-            RegexBasedTerminal inicio_fin = new RegexBasedTerminal("inicio_fin", "%%");
-            RegexBasedTerminal ascii = new RegexBasedTerminal("ascii", "[^\020\013\010\009]");
-            RegexBasedTerminal s_apuntar = new RegexBasedTerminal("s_apuntar", "->");
-            RegexBasedTerminal r_conjunto = new RegexBasedTerminal("r_conjunto", "CONJ");
-            RegexBasedTerminal r_return = new RegexBasedTerminal("r_return", "retorno");
-            RegexBasedTerminal r_texto = new RegexBasedTerminal("r_texto", "yytext");
-            RegexBasedTerminal r_linea = new RegexBasedTerminal("r_linea", "yyline");
-            RegexBasedTerminal r_columna = new RegexBasedTerminal("r_columna", "yyrow");
-            RegexBasedTerminal r_cadena = new RegexBasedTerminal("r_cadena", "string");
-            RegexBasedTerminal r_caracter = new RegexBasedTerminal("r_caracter", "char");
-            RegexBasedTerminal r_entero = new RegexBasedTerminal("r_entero", "entero");
-            RegexBasedTerminal r_flotante = new RegexBasedTerminal("r_flotante", "flotante");
-            RegexBasedTerminal r_booleano = new RegexBasedTerminal("r_boleano", "bool");
-            RegexBasedTerminal r_resev = new RegexBasedTerminal("r_reserv", "RESERV");
+            StringLiteral cadena = new StringLiteral("cadena", "\"");
+            RegexBasedTerminal caracter = new RegexBasedTerminal("cadena", "\"[^\020\013\010\009\042]\"");
+            RegexBasedTerminal rango1 = new RegexBasedTerminal("rango1", "([^\020\013\010\009]|[0-9]+)~([^\020\013\010\009]|[0-9]+)");
+            RegexBasedTerminal rango2 = new RegexBasedTerminal("rango2", "([^\020\013\010\009]|[0-9]+)(,([^\020\013\010\009]|[0-9]+))+");
+            
+
+            var inicio_fin = ToTerm("%%");
+            var s_apuntar = ToTerm("->");
+            var s_colon = ToTerm(":");
+            var s_or = ToTerm("|");
+            var s_mul = ToTerm("*");
+            var s_sum = ToTerm("+");
+            var s_ques = ToTerm("?");
+            var s_dot = ToTerm(".");
+            var s_par_a = ToTerm("(");
+            var s_par_c = ToTerm(")");
+            var s_key_a = ToTerm("[");
+            var s_key_c = ToTerm("]");
+            var s_cor_a = ToTerm("{");
+            var s_cor_c = ToTerm("}");
+            var s_semicolon = ToTerm(";");
+            var s_comma = ToTerm(",");
+            var r_return = ToTerm("retorno");
+            var r_string = ToTerm("string");
+            var r_char = ToTerm("char");
+            var r_int = ToTerm("int");
+            var r_float = ToTerm("float");
+            var r_bool = ToTerm("bool");
+            var r_text = ToTerm("yytext");
+            var r_line = ToTerm("yyline");
+            var r_row = ToTerm("yyrow"); 
+            var r_conj = ToTerm("CONJ");
+            var r_reserv = ToTerm("RESERV");
+            var r_all = ToTerm("[:Todo:]");
+            var r_white = ToTerm("[:Blanco:]");
+            
+
 
             //2. NO TERMINALES
             NonTerminal S0 = new NonTerminal("S0");
@@ -35,17 +60,19 @@ namespace _Compi2_Practica1_201020331
             NonTerminal SENTENCIAS = new NonTerminal("SENTENCIAS");
             NonTerminal SENTENCIA = new NonTerminal("SETENCIA");
             NonTerminal CONJUNTO = new NonTerminal("CONJUNTO");
-            NonTerminal RANGO = new NonTerminal("RANGO");
-            NonTerminal RANGO1 = new NonTerminal("RANGO1");
+            NonTerminal EXPRESIONES = new NonTerminal("EXPRESIONES");
+            NonTerminal EXPRESION = new NonTerminal("EXPRESION");
             NonTerminal RANGO2 = new NonTerminal("RANGO2");
-            NonTerminal ID = new NonTerminal("ID");
             NonTerminal ER = new NonTerminal("ER");
             NonTerminal ER2 = new NonTerminal("ER2");
             NonTerminal ER3 = new NonTerminal("ER3");
+            NonTerminal ID = new NonTerminal("ER3");
             NonTerminal RETURN = new NonTerminal("RETURN");
             NonTerminal RETURN2 = new NonTerminal("RETURN2");
             NonTerminal RETURN3 = new NonTerminal("RETURN3");
             NonTerminal RESERV = new NonTerminal("RESERV");
+            NonTerminal RESERV2 = new NonTerminal("RESERV");
+            NonTerminal RESERV3 = new NonTerminal("RESERV");
 
             //3. PRECEDENCIA DE OPERADORES
 
@@ -56,52 +83,52 @@ namespace _Compi2_Practica1_201020331
 
             LENGUAJE.Rule = inicio_fin + SENTENCIAS + inicio_fin;
 
-            SENTENCIAS.Rule = SENTENCIAS + SENTENCIA
-                                | SENTENCIA;
+            SENTENCIAS.Rule = MakePlusRule(SENTENCIAS, SENTENCIA);
 
-            SENTENCIA.Rule = CONJUNTO;
-                             //| ID + s_apuntar + ER + s_apuntar + RETURN + s_apuntar + RESERV 
-                             //| ID + s_apuntar + ER + s_apuntar + RETURN;
+            SENTENCIA.Rule = CONJUNTO
+                            | EXPRESION;
 
-            CONJUNTO.Rule = r_conjunto + ":" + ID + s_apuntar + RANGO1
-                           | r_conjunto + ":" + ID + s_apuntar + RANGO2;
+            CONJUNTO.Rule = r_conj + s_colon + iden + s_apuntar + rango1 + s_semicolon
+                           | r_conj + s_colon + iden + s_apuntar + rango2 + s_semicolon;
+                           
+            EXPRESION.Rule = iden + s_apuntar + ER + s_apuntar + RETURN  + s_semicolon
+                            |iden + s_apuntar + ER + s_apuntar + RETURN + s_apuntar+ RESERV + s_semicolon ;
 
-            RANGO1.Rule = ascii + "~" + ascii;
+            ER.Rule = ER2
+                      | ER3
+                      | ID;
 
-            RANGO2.Rule = MakeListRule(RANGO2, ToTerm(","), ascii);
+            ER2.Rule = ER + ER + s_or
+                | ER + ER + s_dot;
 
-            ER.Rule = ER + "|" + ER2
-                      | ER + ER2 + "|"
-                      | ER + ER2 + "."
-                      | ER2;
+            ER3.Rule = ER + s_mul
+                 | ER + s_sum
+                 | ER + s_ques;         
+            
 
-            ER2.Rule = ER2 + ER3 + "+"
-                        | ER2 + ER3 + "*"
-                        | ER3 + "?"
-                        | ER3;
-
-            ER3.Rule = ID
-                       | RANGO
-                       | ascii;
-
-            RETURN.Rule = r_return + "(" + ID + "," + RETURN2 + ")";
-
-            RETURN2.Rule = MakeListRule(RETURN2, ToTerm(","), RETURN3);
-
-            RETURN3.Rule =    r_texto
-                            | r_linea
-                            | r_columna
-                            | r_cadena
-                            | r_caracter
-                            | r_entero
-                            | r_flotante
-                            | r_booleano;
-                            
-            RESERV.Rule = r_resev + "[" + SENTENCIAS + "]";
-             
             ID.Rule = iden
-                     | "\"" + iden + "\""
-                     | "\"" + ascii + "\"";
+                      | caracter
+                      | rango1
+                      | rango2;
+
+            RETURN.Rule = r_return + s_par_a + iden + s_comma + RETURN2 + s_par_c;
+
+            RETURN2.Rule = MakeListRule(RETURN2, s_comma, RETURN3);
+
+            RETURN3.Rule = r_text
+                            | r_line
+                            | r_row
+                            | r_string
+                            | r_char
+                            | r_float
+                            | r_int
+                            | r_bool;
+
+            RESERV.Rule = r_reserv + s_apuntar + s_key_a + RESERV2 + s_key_c;
+
+            RESERV2.Rule = MakePlusRule(RESERV2, RESERV3);
+
+            RESERV3.Rule = cadena + s_apuntar + RETURN + s_semicolon;
 
             this.Root = S0;
         }
